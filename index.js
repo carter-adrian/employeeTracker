c04779481f36b7a96cdf64aa13cb8291a60dcc21
unknown <carter.adrian@gmail.com>
2023-02-05 16:26:52 -0500

    viewAll function

--- index.js
+++ index.js
@@ -1,88 +1,114 @@
-// Establish variables
 require('dotenv').config();
 const mysql2 = require('mysql2');
-const inquirer = require('inquirer');
+const inquier = require('inquirer');
 const cTable = require('console.table');
 const figlet = require('figlet');
 
-// Set up .env 
 const connection = mysql2.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: process.env.DB_PW,
-    database: 'employee_DB',
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.DB_PW,
+  database: 'employee_DB',
 });
 
-// Connect to DB
+// Connect to the DB
 connection.connect((err) => {
-    if (err) throw err;
-    console.log(`Connected as id ${connection.threadId}\n`);
-    figlet(`Employee Tracker`, function(err, data) {
-        if (err) {
-            console.log(`ascil art not loaded`);
-            return;
-        } else {
-            console.log(data);
-        }
-        startPrompt();
-    });
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}\n`);
+  figlet('Employee tracker', function(err, data) {
+    if (err) {
+      console.log('ascii art not loaded');
+      return 
+    } else {
+      console.log(data);
+    }  
+    startPrompt();
+  });
 });
 
-function startPrompt();{
-    const startQuestion = [{
-        type: "list",
-        name: "action",
-        message: "what would you like to do?",
-        loop: false,
-        choices: ["View all employees", "View all roles", "View all departments", "add an employee", "add a role", "add a department", "update role for an employee", "update employee's manager", "view employees by manager", "delete a department", "delete a role", "delete an employee", "View the total utilized budget of a department", "quit"]
-       }]
-
-
-       inquier.prompt(startQuestion)
-       .then(response => {
-         switch (response.action) {
-           case "View all employees":
-             viewAll("EMPLOYEE");
-             break;
-           case "View all roles":
-             viewAll("ROLE");
-             break;
-           case "View all departments":
-             viewAll("DEPARTMENT");
-             break;
-           case "add a department":
-             addNewDepartment();
-             break;
-           case "add a role":
-             addNewRole();
-             break;
-           case "add an employee":
-             addNewEmployee();
-             break;
-           case "update role for an employee":
-             updateRole();
-             break;
-           case "view employees by manager":
-             viewEmployeeByManager();
-             break;
-           case "update employee's manager":
-             updateManager();
-             break;
-           case "delete a department":
-             deleteDepartment();
-             break;
-           case "delete a role":
-             deleteRole();
-             break;
-           case "delete an employee":
-             deleteEmployee();
-             break;
-           case "View the total utilized budget of a department":
-             viewBudget();
-             break;
-           default:
-             connection.end();
-         }
-       });
+function startPrompt() {
+  const startQuestion = [{
+    type: "list",
+    name: "action",
+    message: "what would you like to do?",
+    loop: false,
+    choices: ["View all employees", "View all roles", "View all departments", "add an employee", "add a role", "add a department", "update role for an employee", "update employee's manager", "view employees by manager", "delete a department", "delete a role", "delete an employee", "View the total utilized budget of a department", "quit"]
+  }]
+  
+  inquier.prompt(startQuestion)
+  .then(response => {
+    switch (response.action) {
+      case "View all employees":
+        viewAll("EMPLOYEE");
+        break;
+      case "View all roles":
+        viewAll("ROLE");
+        break;
+      case "View all departments":
+        viewAll("DEPARTMENT");
+        break;
+      case "add a department":
+        addNewDepartment();
+        break;
+      case "add a role":
+        addNewRole();
+        break;
+      case "add an employee":
+        addNewEmployee();
+        break;
+      case "update role for an employee":
+        updateRole();
+        break;
+      case "view employees by manager":
+        viewEmployeeByManager();
+        break;
+      case "update employee's manager":
+        updateManager();
+        break;
+      case "delete a department":
+        deleteDepartment();
+        break;
+      case "delete a role":
+        deleteRole();
+        break;
+      case "delete an employee":
+        deleteEmployee();
+        break;
+      case "View the total utilized budget of a department":
+        viewBudget();
+        break;
+      default:
+        connection.end();
+    }
+  })
+  .catch(err => {
+    console.error(err);
+  });
 }
+
+
+const viewAll = (table) => {
+  // const query = `SELECT * FROM ${table}`;
+  let query;
+  if (table === "DEPARTMENT") {
+    query = `SELECT * FROM DEPARTMENT`;
+  } else if (table === "ROLE") {
+    query = `SELECT R.id AS id, title, salary, D.name AS department
+    FROM ROLE AS R LEFT JOIN DEPARTMENT AS D
+    ON R.department_id = D.id;`;
+  } else {//employee
+    query = `SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, 
+    R.title AS role, D.name AS department, CONCAT(M.first_name, " ", M.last_name) AS manager
+    FROM EMPLOYEE AS E LEFT JOIN ROLE AS R ON E.role_id = R.id
+    LEFT JOIN DEPARTMENT AS D ON R.department_id = D.id
+    LEFT JOIN EMPLOYEE AS M ON E.manager_id = M.id;`;
+
+  }
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+
+    startPrompt();
+  });
+};
